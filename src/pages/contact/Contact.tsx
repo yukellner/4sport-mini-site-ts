d923b0ed027275c4323046b343a5931bdcf4cb27
Yuval Kellner <yukellner@gmail.com>
2022-08-29 18:44:51 +0300

    contact form to server

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,38 +1,79 @@
-import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material"
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextareaAutosize, TextField } from "@mui/material"
 import { useState } from "react"
 import { HeaderTitle } from "../../components/HeaderTitle"
 import { RaceObjModel } from "../../models/raceObj.model"
 
-export const Contact:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
+export const Contact: React.FC<{ eventObj: RaceObjModel }> = ({ eventObj }) => {
+
 
     const [newStay, setNewStay] = useState(
         {
-            firstName: null,
-            lastName: null,
+            fullName: null,
             roll: null,
+            phoneNumber: null,
             email: null,
-            competition: eventObj.description
+            competition: eventObj.description,
+            content: null
 
 
         })
 
 
 
-    const handleChange = (ev:any) => {
+    const handleChange = (ev: any) => {
         ev.preventDefault()
         const field = ev.target.name
         const value = ev.target.value
-        console.log(field)
-        console.log(value)
+        // console.log(field)
+        // console.log(value)
         setNewStay({ ...newStay, [field]: value })
 
         // alert('submit')
     }
 
-    const onSubmit = (ev:any) => {
+    const onSubmit = async (ev: any) => {
         ev.preventDefault()
-        console.log('state', newStay)
+
+        try {
+            const response = await fetch('https://www.4sport-live.com/miniSite/contact/?eventId=1', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: newStay.fullName,
+                    mobile: newStay.phoneNumber,
+                    email: newStay.email,
+                    content: newStay.content
+                })
+            })
+            return response
+        } catch (error) {
+            console.error(error);
+        }
+
+
     }
+
+    // const getJsonFromApi = async () => {
+    //     //http://hinawi:3000/
+    //     try {
+    //         const getJsonFromApi = async () => {
+    //             //http://hinawi:3000/
+    //             try {
+    //                 const response = await fetch('https://www.4sport-live.com/miniSite/eventData/?comp=3432');
+    //                 const responseJson = await response.json();
+    //                 setEventObj(Object(responseJson))
+    //                 console.log(Object(responseJson))
+    //                 return responseJson;
+    //             } catch (error) {
+    //                 console.error(error);
+    //             }
+    //         } const responseJson = await response.json();
+    //         setEventObj(Object(responseJson))
+    //         console.log(Object(responseJson))
+    //         return responseJson;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
 
 
 
@@ -48,9 +89,10 @@ export const Contact:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
                 <div className="contact-form">
                     <form id="contact-form" onSubmit={onSubmit} method="POST">
                         <div className="form-group">
-                            <TextField onChange={(ev) => handleChange(ev)} id="outlined-basic" label="שם פרטי" name="firstName" variant="outlined" InputLabelProps={{ style: { color: '#222222' } }} />
-                            <TextField onChange={(ev) => handleChange(ev)} id="outlined-basic" label="שם משפחה" name="lastName" variant="outlined" InputLabelProps={{ style: { color: '#222222' } }} />
+                            <TextField onChange={(ev) => handleChange(ev)} id="outlined-basic" label="שם מלא" name="fullName" variant="outlined" InputLabelProps={{ style: { color: '#222222' } }} />
+                            <TextField onChange={(ev) => handleChange(ev)} id="outlined-basic" label="מספר טלפון" name="phoneNumber" variant="outlined" InputLabelProps={{ style: { color: '#222222' } }} />
                             <TextField onChange={(ev) => handleChange(ev)} id="outlined-basic" label="כתובת אימייל" name="email" variant="outlined" InputLabelProps={{ style: { color: '#222222' } }} />
+                            <TextareaAutosize onChange={(ev) => handleChange(ev)}  aria-label="תוכן" name="content"  style= {{ color: '#222222', width: 200 }}   placeholder="Maximum 4 rows"/>
 
 
                         </div>
