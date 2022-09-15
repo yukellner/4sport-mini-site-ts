30df313e28bb948086780c28455924db9d046454
Yuval Kellner <yukellner@gmail.com>
2022-09-15 12:58:53 +0300

    waze improved

--- src/pages/details/details-cmp/sections/location.tsx
+++ src/pages/details/details-cmp/sections/location.tsx
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { RaceObjModel } from "../../../../models/raceObj.model"
+// import { faWater, brands  } from '@fortawesome/free-solid-svg-icons'
+import { faWaze } from '@fortawesome/free-brands-svg-icons'
 
 
 
-export const Location:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
+
+export const Location: React.FC<{ eventObj: RaceObjModel }> = ({ eventObj }) => {
 
 
 
@@ -11,8 +15,10 @@ export const Location:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
     return (
         <div className="location-container">
             <div className="meeting-point">
+
                 <a href={eventObj.wazeDirection}>
-                    <img src={require('../../../../assets/icons/icon-waze.png')} alt="" />
+                    <FontAwesomeIcon href={eventObj.wazeDirection} className="icon" icon={faWaze} />
+                    {/* <img src={require('../../../../assets/icons/icon-waze.png')} alt="" /> */}
                     <h3>לחץ לניווט לשטח הכינוס</h3>
                 </a>
 
