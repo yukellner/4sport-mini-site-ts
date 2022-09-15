8b30f8ad84ff0e1c13e48947b013103abb0c9f8a
Yuval Kellner <yukellner@gmail.com>
2022-09-15 11:36:32 +0300

    footer improved

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,13 @@ import Favicon from "react-favicon";
 function App() {
     const [eventObj, setEventObj] = useState<RaceObjModel | any>(null)
     const [codeName, setCodeName] = useState<string | null>(null)
-    
+
 
 
     function getCodeName(): string {
         const urlSegments = window.location.href.split('/')
         const siteSegmentIndex = urlSegments.indexOf('site')
-        console.log('path',urlSegments[siteSegmentIndex + 1])
+        console.log('path', urlSegments[siteSegmentIndex + 1])
         return urlSegments[siteSegmentIndex + 1]
     }
 
@@ -42,7 +42,7 @@ function App() {
             const responseJson = await response.json();
             setEventObj(Object(responseJson))
             console.log(Object(responseJson))
-            
+
 
             return responseJson;
         } catch (error) {
@@ -50,33 +50,33 @@ function App() {
         }
     }
 
-    if(!eventObj) return <div className="loader"></div>
+    if (!eventObj) return <div className="loader"></div>
     document.title = eventObj.description
 
     const basePath = `/site/${codeName}`
     return (
         <BrowserRouter >
-            <ScrollToTop/>
+            <ScrollToTop />
             <Favicon url={eventObj.logo}></Favicon>
 
 
-           
+
 
             <div className="App">
                 <AppHeader eventObj={eventObj} />
                 <Routes>
-                    <Route path={`${basePath}/`} element={<Home eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/contact`} element={<Contact eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/enrollment`} element={<Enrollment eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/details`} element={<Details eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/maps`} element={<Maps eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/gallery`} element={<Gallery eventObj={eventObj}/>} />
-                    <Route path={`${basePath}/info`} element={<Info eventObj={eventObj}/>} />
+                    <Route path={`${basePath}/`} element={<Home eventObj={eventObj} />} />
+                    <Route path={`${basePath}/contact`} element={<Contact eventObj={eventObj} />} />
+                    <Route path={`${basePath}/enrollment`} element={<Enrollment eventObj={eventObj} />} />
+                    <Route path={`${basePath}/details`} element={<Details eventObj={eventObj} />} />
+                    <Route path={`${basePath}/maps`} element={<Maps eventObj={eventObj} />} />
+                    <Route path={`${basePath}/gallery`} element={<Gallery eventObj={eventObj} />} />
+                    <Route path={`${basePath}/info`} element={<Info eventObj={eventObj} />} />
                 </Routes>
+                {eventObj.sponsors && eventObj.sponsors.length !== 0 && <Sponsers eventObj={eventObj} />}
+                <AppFooter eventObj={eventObj} />
             </div>
 
-            {eventObj.sponsors && eventObj.sponsors.length !== 0 && <Sponsers eventObj={eventObj}/>}
-            <AppFooter eventObj={eventObj}/>
         </BrowserRouter>
     );
 }
