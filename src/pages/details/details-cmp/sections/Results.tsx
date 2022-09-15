6e7bcbc1c23a30bded0fca4d8c233f19e218fc38
Yuval Kellner <yukellner@gmail.com>
2022-09-15 18:20:09 +0300

    details header

--- src/pages/details/details-cmp/sections/Results.tsx
+++ src/pages/details/details-cmp/sections/Results.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@mui/material"
 import { RaceObjModel } from "../../../../models/raceObj.model"
 
 
 
-export const Results:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
+export const Results: React.FC<{ eventObj: RaceObjModel }> = ({ eventObj }) => {
 
 
 
@@ -10,16 +11,11 @@ export const Results:React.FC<{eventObj:RaceObjModel}> = ({ eventObj }) => {
 
     return (
         <div className="resaluts-container">
-
-<div>
-                <h1>הערות</h1>
-                <p>
-
-                    {eventObj.comments}
-
-                </p>
+            <div>
+                <h1>תוצאות</h1>
+                <Button className="sign-btn" href={eventObj.registrationUrl} variant="contained">לחץ לתוצאות</Button>
             </div>
-           
+
 
 
         </div >
