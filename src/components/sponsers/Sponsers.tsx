44ffbab683f778d6e5f568adb291f2d472c396cd
Yuval Kellner <yukellner@gmail.com>
2022-09-16 16:54:31 +0300

    sponsers styling update

--- src/components/sponsers/Sponsers.tsx
+++ src/components/sponsers/Sponsers.tsx
@@ -6,11 +6,20 @@ import { SponserCard } from "./componets/sponser-card/SponserCard"
 export const Sponsers:React.FC<{eventObj:RaceObjModel}> = ({eventObj}) => {
 
     useEffect(() => {
+        updateFooteStyling()
     }, [])
+
+    const updateFooteStyling = () =>{
+
+            const element = document.getElementById("footer-container");
+            const el1: HTMLElement = element!;
+            el1.classList.remove("margin-top-auto");
+        
+    }
 
     return (
         
-        <div>
+        <div className="sponsers-main-container">
             <h1 className="header-large">נותני החסות</h1>
             <div className="sponsers-container">
                 {eventObj.sponsors.map(sponser =>
