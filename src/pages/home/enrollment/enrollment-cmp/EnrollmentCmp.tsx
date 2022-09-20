30e5cc6b760e5f6769592983c6f28656df2c2eb2
Yuval Kellner <yukellner@gmail.com>
2022-09-20 15:00:34 +0300

    Design improved

--- src/pages/home/enrollment/enrollment-cmp/EnrollmentCmp.tsx
+++ src/pages/home/enrollment/enrollment-cmp/EnrollmentCmp.tsx
@@ -1,17 +1,45 @@
+import styled from "@emotion/styled";
 import { Button } from "@mui/material"
 import { RaceObjModel } from "../../../../models/raceObj.model"
 
 
 export const EnrollmentCmp: React.FC<{ eventObj: RaceObjModel }> = ({ eventObj }) => {
+
+    const StyledButton = styled(Button)`
+    background-color: var(--BackgroundColorJson);
+    color: var(--fontColorJson);
+    border: unset;
+    box-sizing: border-box;
+    margin-top: 1rem;
+    &:hover {
+            box-sizing: border-box;
+        border: unset;
+      background-color: ${eventObj.foregroundColor};
+      color:${eventObj.backgroundColor};
+    //   border:1px ${eventObj.backgroundColor} solid;
+    }
+    // &:focus {
+    //   background-color: green;
+    // }
+  `;
+
+
     return (
         <div className="main-enrollment vh-80">
             <div className="background-home"></div>
+            <div className="dark-home"></div>
             <div className="header-home">
                 <div>
-                {/* kenburns-top */}
-                <h1 className="scale-up-center">{eventObj.description}</h1>
+                    {/* kenburns-top */}
+                    {/* className="scale-up-center" */}
+                    <h1 >{eventObj.description}</h1>
+                    <h2 className="hero">ההרשמה בעיצומה!</h2>
                 </div>
-                {/* <Button className="button-border" variant="contained">הרשמה</Button> */}
+                <StyledButton className="sign-btn" href={eventObj.registrationUrl}
+                    sx={{}}
+                    variant="outlined">
+                    להרשמה
+                </StyledButton>
             </div>
 
         </div>
