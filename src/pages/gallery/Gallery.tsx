6969494d2d12d0850ed210c5a39310368cc70832
Yuval Kellner <yukellner@gmail.com>
2022-09-26 15:30:13 +0300

    galley api prottection

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -7,15 +7,18 @@ import { HeaderTitle } from "../../components/HeaderTitle";
 export const Gallery: React.FC<{ eventObj: RaceObjModel, pageHeader: Function }> = ({ eventObj, pageHeader }) => {
 
   const [galleryImages, setGalleryItems] = useState<GalleryModel>()
+  const [galleryJsonReceived, setGalleryJsonReceived] = useState(true)
 
   const getItemsFromJson = async (codeName: string) => {
     try {
       const response = await fetch(`https://www.minisite.4sport-services.com/v1/gallery/${eventObj.eventId}`);
       const responseJson = await response.json();
+      setGalleryJsonReceived(true)
       setGalleryItems(responseJson)
       console.log(Object(responseJson))
       return responseJson;
     } catch (error) {
+      setGalleryJsonReceived(false)
       console.error(error);
     }
   }
@@ -28,17 +31,14 @@ export const Gallery: React.FC<{ eventObj: RaceObjModel, pageHeader: Function }>
   if (!galleryImages) return (
     <div className="min-height">
       <HeaderTitle titleText={'גלריה'} />
-      <div style={{ top: '30%' }} className="lds-ripple"><div></div><div></div></div>
+      {galleryJsonReceived ? <div style={{ top: '30%' }} className="lds-ripple"><div></div><div></div></div> : <h1>תמונות יעלו בקרוב</h1>}
     </div>
   )
 
   return (
     <div className="main-gallery min-height">
       <HeaderTitle titleText={'גלריה'} />
-
       {galleryImages.gallery.length > 1 ? <WSPGallery galleryImages={galleryImages} /> : <h1>תמונות יעלו בקרוב</h1>}
-
-
     </div >
   )
 
