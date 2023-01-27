d03896b3c5f22dbab737c521de6ad19ab97d6b03
Yuval Kellner <yukellner@gmail.com>
2023-01-27 15:52:11 +0200

    footer year update

--- src/components/footer/AppFooter.tsx
+++ src/components/footer/AppFooter.tsx
@@ -1,12 +1,14 @@
 import { IEvent } from "../../models/Event"
 import React from "react";
 
+const year = new Date().getFullYear() 
+
 export const AppFooter: React.FC<{event:Event}> = ({event}) => {
     return (
         <div id="footer-container" className="footer-container margin-top-auto">
             <div className="footer">
                 <a href="http://www.4sport.co.il">
-                    2022 כל הזכויות שמורות ל4ספורט בע״מ
+                    {year} כל הזכויות שמורות ל4ספורט בע״מ
                 </a>
             </div>
         </div>
