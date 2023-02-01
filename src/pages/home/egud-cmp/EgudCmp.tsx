014c5a84f5fc931366bb6cbab69af699323fe47a
Yuval Kellner <yukellner@gmail.com>
2023-02-01 18:38:08 +0200

    resize igud tav img

--- src/pages/home/egud-cmp/EgudCmp.tsx
+++ src/pages/home/egud-cmp/EgudCmp.tsx
@@ -7,12 +7,12 @@ export const EgudCmp: React.FC<{ event: IEvent }> = ({ event }) => {
     return (
         <div className="egud-box">
             <div className="left-side">
-                <img src="https://www.4sport-live.com/media/minisite/7/ydxaUTOYQNwIaPei_Tav_Igud_Haatletika.png" alt="" />
+                <img src="https://www.4sport-live.com/media/minisite/7/SeSCIdbWPhoeYECc_rsz_1tav_igud_haatletika.png" alt="" />
             </div>
             <div className="right-side">
                 <h2>
-                    המרוץ מוכר כבעל תקן באיגוד האתלטיקה ולכן זוכה לסיוע מקצועי צמוד והכרה רשמית בתוצאותיו.
-                    מדידת מסלול המרוץ נעשית ע"י מודד מוסמך מטעם ה - AIMS.
+                המרוץ מוכר כבעל תקן באיגוד האתלטיקה ולכן זוכה לסיוע מקצועי צמוד והכרה רשמית בתוצאותיו.
+                מדידת מסלול המרוץ נעשית ע"י מודד מוסמך מטעם ה - AIMS.
                 </h2>
             </div>
         </div>
