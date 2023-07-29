226b94cc7b0da19c84578bd4327884f4192e52c8
omrikellner <omrikellner6@gmail.com>
2023-07-29 19:13:17 +0300

    fix prize modal

--- src/pages/details/details-cmp/sections/prize/Prize.tsx
+++ src/pages/details/details-cmp/sections/prize/Prize.tsx
@@ -4,7 +4,6 @@ import {PrizeTable} from "../PrizeTable";
 import {PrizeDetails} from "./components/prizeTable/PrizeDetails";
 
 export const Prize: React.FC<{ event: IEvent }> = ({ event }) => {
-    console.log(event,"event")
     return (
         <div className="resaluts-container">
             <>
@@ -15,20 +14,7 @@ export const Prize: React.FC<{ event: IEvent }> = ({ event }) => {
                     <div style={{width: '100%'}}>
                         <h1 style={{width: '100%', textAlign: 'center', color: 'white'}}>{heat.description}</h1>
                         <div style={{width: '100%', height: '1px', background: 'white', marginTop: '8px', marginBottom: '8px'}}></div>
-
-
-
                         <PrizeDetails prizes={event.heats[0].prizes}/>
-
-
-
-
-                        {/*<div style={{float: 'right'}}>*/}
-                        {/*    <PrizeTable  prizes={event.heats[0].prizes} gender={'M'}/>*/}
-                        {/*</div>*/}
-                        {/*<div style={{float: 'left'}}>*/}
-                        {/*    <PrizeTable prizes={event.heats[0].prizes} gender={'F'}/>*/}
-                        {/*</div>*/}
                     </div>
                 )}
             </>
