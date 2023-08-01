2eafa36e95a4312062e0dfa8e2617c1f8669f294
omrikellner <omrikellner6@gmail.com>
2023-08-01 08:32:28 +0300

    fix category modal

--- src/pages/details/details-cmp/sections/Category.tsx
+++ src/pages/details/details-cmp/sections/Category.tsx
@@ -6,11 +6,13 @@ export const Category: React.FC<{ event: IEvent }> = ({ event }) => {
         <div className="resaluts-container">
             <div className="categories">
                 <h1>קטגוריות</h1>
-                {event.heats.map(heat =>
-                    <div key={heat.description}>
-                        {!heat.amami && <h3 style={{fontSize:24}}>מקצה {heat.description}</h3>}
-                        {!heat.amami && heat.sections.map(section => <h3 style={{fontSize:20}}
-                            key={section.description}>{section.description}</h3>)}
+                {event.heats.map((heat,index) =>
+                    <div key={index} style={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+                        {!heat.amami && <div style={{paddingBlock:10,marginBottom:5,borderBottom:"1px solid white",marginTop:10,fontSize:24,width: '100%', textAlign: 'center', color: 'white'}}>מקצה {heat.description}</div>}
+                        {!heat.amami && heat.sections.map((section,index) =>
+                            <div style={{fontSize:22,paddingBlock:2}} key={index}>
+                                {section.description}
+                            </div>)}
                         <br />
                     </div>
                 )}
